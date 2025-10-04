8f5c8ef068d5a3005b1d929af974fd12a70e8171
Shashwat <forwardtoshashwat@gmail.com>
2025-10-04 12:33:20 +0530

    Create remove_element.py

---
+++ Shashwat/Array/remove_element.py
@@ -0,0 +1,14 @@
+'''
+https://leetcode.com/problems/remove-element/description/
+'''
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        slow = 0
+        fast = 0
+        while fast<len(nums):
+            if nums[fast] != val:
+                nums[slow]=nums[fast]
+                slow+=1
+            fast+=1
+        return slow
